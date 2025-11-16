f90ebfe38c783a8dbebe3d164bb722156c3e7dfb
WhoDoesFluttershyLike <mikaasaa@mail.ru>
2025-11-16 16:34:52 +0600

    test auth-service

--- auth-service/internal/http/handlers.go
+++ auth-service/internal/http/handlers.go
@@ -1,24 +1,50 @@
 package http
 
-import(
+import (
 	"net/http"
-
-    "github.com/gin-gonic/gin"
 	"github.com/WhoDoesFluttershyLike/Fitness-Workout-Tracker/auth-service/internal/service"
+	"github.com/gin-gonic/gin"
 )
 
 type Handler struct {
+    svc *service.UserService
 }
 
-func NewHandler() *Handler {
-    return &Handler{}
+type RegisterInput struct {
+    Name     string `json:"name"`
+    Email    string `json:"email"`
+    Password string `json:"password"`
+}
+
+func NewHandler(s *service.UserService) *Handler {
+    return &Handler{svc: s}
 }
 
 func (h *Handler) Register(c *gin.Context) {
-   
+    var input RegisterInput
+    if err := c.ShouldBindJSON(&input); err != nil {
+        c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+        return
+    }
+
+    if err := h.svc.Register(input.Name, input.Email, input.Password); err != nil {
+        c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+        return
+    }
     c.JSON(http.StatusOK, gin.H{"message": "register endpoint"})
 }
 
 func (h *Handler) Login(c *gin.Context) {
-    c.JSON(http.StatusOK, gin.H{"message": "login endpoint"})
+    var input RegisterInput
+    if err := c.ShouldBindJSON(&input); err != nil {
+        c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+        return
+    }
+
+    token, err := h.svc.Login(input.Email, input.Password)
+    if err != nil {
+        c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+        return
+    }
+    c.JSON(http.StatusOK, gin.H{"token": token})
 }
--- auth-service/internal/repository/user_repository.go
+++ auth-service/internal/repository/user_repository.go
@@ -1,6 +1,8 @@
 package repository
 
 import (
+	"errors"
+
 	"github.com/WhoDoesFluttershyLike/Fitness-Workout-Tracker/auth-service/internal/models"
 	"gorm.io/gorm"
 )
@@ -15,21 +17,30 @@ type userRepository struct {
 	db *gorm.DB
 }
 
-// Create implements UserRepository.
-func (u *userRepository) Create(user *models.User) error {
-	panic("unimplemented")
-}
-
-// GetByEmail implements UserRepository.
-func (u *userRepository) GetByEmail(email string) (*models.User, error) {
-	panic("unimplemented")
-}
-
-// GetById implements UserRepository.
-func (u *userRepository) GetById(id uint) (*models.User, error) {
-	panic("unimplemented")
-}
-
 func NewUserRepository(db *gorm.DB) UserRepository {
 	return &userRepository{db: db}
+}
+
+func (u *userRepository) Create(user *models.User) error {
+	return u.db.Create(user).Error
+}
+
+func (u *userRepository) GetByEmail(email string) (*models.User, error) {
+	var user models.User
+	err := u.db.Where("email = ?", email).First(&user).Error
+
+	if errors.Is(err, gorm.ErrRecordNotFound) {
+		return nil, nil
+	}
+	return &user, err
+}
+
+func (u *userRepository) GetById(id uint) (*models.User, error) {
+	var user models.User
+	err := u.db.Where("id = ?", id).First(&user).Error
+
+	if errors.Is(err, gorm.ErrRecordNotFound) {
+		return nil, nil
+	}
+	return &user, err
 }
